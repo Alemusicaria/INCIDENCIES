6f104735235a973c709b6b8124e9afe95dbc3d36
steve <stevejph48@gmail.com>
2024-10-25 13:26:21 +0200

    mensaje

---
+++ app/views/Forms/V_Login.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+    <!-- Custom CSS File link -->
+    <link rel="stylesheet" href="css/styles.css?v=1"> 
+
+</head>
+<body>
+
+    <!-- Logo Header -->
+    <div class="header"> 
+        <img src="Images/Login/la_salle_white.jpeg" alt="">
+    </div>
+
+    <section class="home" id="home">
+        <div class="container">
+            <h1>Inicia Sessió</h1>
+            <br>
+            <a>Nom d'usuari<a/>
+            <a>Contrasenya</a>
+            <br>
+            <a>Inicia la sessió</a>
+            <a>¿Has oblitad el teu nom d' usuari o contrsenya?</a>
+            <a>Aqui puedes registrarte aaaaaa</a>
+        </div>
+    </section>
+
+    <!-- Logo Footer -->
+    <div class="footer"> 
+        <img src="Images/Login/Salleguard.png" alt="">
+    </div>
+    
+
+    <!-- jQuery cdn link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!-- Custom JS file link -->
+    <script src="js/main.js"></script>
+
+</body>
+</html>
