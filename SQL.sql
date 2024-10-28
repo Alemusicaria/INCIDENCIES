37c57c9f4cbc5ba6ce47078aba72cdf5f8ca08a6
Alemusicaria <aleixpratmarin@gmail.com>
2024-10-28 11:21:43 +0100

    sql

---
+++ SQL.sql
@@ -0,0 +1,25 @@
+CREATE TABLE usuaris (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom_cognoms VARCHAR(100) NOT NULL,
+    correu VARCHAR(100) NOT NULL UNIQUE,
+    contrasenya VARCHAR(255) NOT NULL,
+    telefon VARCHAR(20),
+    rol ENUM('Professor', 'Tecnic', 'Admin') NOT NULL,
+    habilitat BOOLEAN DEFAULT TRUE,
+    data_registre TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE incidencies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    creador_nom_cognoms VARCHAR(100) NOT NULL,
+    reporter_nom_cognoms VARCHAR(100),
+    reporter_email VARCHAR(100),
+    titol_fallo VARCHAR(150),
+    descripcio TEXT NOT NULL,
+    tipus_incidencia ENUM('Calefacció', 'Electricitat/Fontaner', 'Informàtica', 'Fusteria', 'Ferrer', 'Obres', 'Audiovisual', 'Equips de seguretat', 'Neteja de clavegueram') NOT NULL,
+    ubicacio VARCHAR(100),
+    data_incidencia TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estat ENUM('Pendent', 'En Progrés', 'Resolta') DEFAULT 'Pendent',
+    prioritat ENUM('Baixa', 'Mitjana', 'Alta') DEFAULT 'Baixa',
+    imatges TEXT
+);
