ac0c69138cc17eef39764a03a147ae7d7890b0c0
Leo0593 <leandroclavijo854@gmail.com>
2024-11-05 11:20:55 +0100

    sadasd

--- app/controllers/C_Login.php
+++ app/controllers/C_Login.php
@@ -1,16 +1,36 @@
 <?php
+require 'app/models/Login.php';
 
-require("Models/M_Login.php");
-
-class C_Login {
-    
-    public function __construct() {
+class C_Login
+{
+    public function verificar_login()
+    {
+        $login = new login();
+        if ($login->verificar_login()) {
+            // Si el login es exitoso, redirige a la página de inicio
+            header( "Location: /index.php?controller=Login&method=bienvenido" );
+            exit;
+        } else {
+            // Si hubo un error, redirige al formulario de login
+            header("Location: /index.php?controller=Login&method=login");
+            exit;
+        }
     }
 
-    public function mostrarLogin() {
-        require_once("Views/Formularios/V_Login.php");
+    public function login()
+    {
+        require 'app/views/formularios/V_Login.php';
     }
 
+    public function bienvenido()
+    {
+        require 'app/views/formularios/V_Principal.php';
+    }
+
+    public function cerrar_sesion()
+    {
+        session_destroy();
+        header("Location: /index.php?controller=Login&method=login");
+        exit;
+    }
 }
-
-?>
