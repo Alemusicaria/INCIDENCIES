a8ddf27ca61f910ba015cf1cbd6eff48e7bb4ef8
steve <stevejph48@gmail.com>
2024-11-07 13:24:55 +0100

    dsa

--- PROVA/index.php
+++ PROVA/index.php
@@ -4,6 +4,11 @@
 <!------------------------------------------------------------------------>
 <!------------------------------------------------------------------------>
 
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html lang="ca">
 
@@ -24,49 +29,10 @@
 
 <body>
     <div class="wrapper">
-        <aside id="sidebar">
-            <div class="d-flex">
-                <button class="toggle-btn" type="button">
-                    <i class="lni lni-grid-alt"></i>
-                </button>
-                <div class="sidebar-logo">
-                    <a href="#">Nom usuari</a>
-                </div>
-            </div>
-            <ul class="sidebar-nav">
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-user"></i>
-                        <span>Perfil</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-home"></i>
-                        <span>Inici</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-agenda"></i>
-                        <span>Les meves incidències</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-cog"></i>
-                        <span>Gestionar professor</span>
-                    </a>
-                </li>
-            </ul>
-            <div class="sidebar-footer">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-exit"></i>
-                    <span>Sortir</span>
-                </a>
-            </div>
-        </aside>
-
+        <?php
+        include("sidebar.php"); // Aquí se incluye la barra lateral
+        ?>
+        
         <!------------------------------------------------------------------------->
         <!------------------------------------------------------------------------->
         <!--------------------------        INICI        -------------------------->
