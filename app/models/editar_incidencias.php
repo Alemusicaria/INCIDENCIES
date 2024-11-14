ad8032ca667d31c0dd59b03e39c32c9b686012e3
Leo0593 <leandroclavijo854@gmail.com>
2024-11-14 09:23:52 +0100

    dasdsad

--- app/models/editar_incidencias.php
+++ app/models/editar_incidencias.php
@@ -18,7 +18,20 @@ class editar_incidencias
             }
 
             if (isset($_POST['id']) && is_numeric($_POST['id'])) {
-                
+                $codigo = $_POST['id'];
+
+                $query = "SELECT * FROM incidencies WHERE id = $codigo";
+                $result = $mysql->query($query);
+
+                if($reg = $result->fetch_array())
+                {
+                    require "app/views/layouts/Forms/V_ActualizarIncidencia.php";
+                }else {
+                    echo "No se ha encontrado la incidencia con el ID " . htmlspecialchars($codigo);
+                }
+            } else{
+                echo "No se ha especificado un ID válido.";
+                return false;
             }
     }
             
