791b6d879303028154d19ad654548551b4a23724
steve <stevejph48@gmail.com>
2024-11-15 18:53:38 +0100

    dfs

--- app/views/layouts/menu/menu.php
+++ app/views/layouts/menu/menu.php
@@ -91,8 +91,9 @@
 
 <!-- Barra lateral -->
 <aside id="sidebar">
-    <div class="sidebar-space">
-    </div>
+
+    <!--<div class="sidebar-space"></div>-->
+
     <ul class="sidebar-nav">
         <div class="sidebar-perfil">
             <div class="sidebar-perfil-img">
