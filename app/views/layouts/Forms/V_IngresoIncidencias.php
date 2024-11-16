68f7f5058f7052d3e506cb7bcc627e9775f7014f
Leo0593 <leandroclavijo854@gmail.com>
2024-11-16 20:20:41 +0100

    asdasdas

--- app/views/layouts/Forms/V_IngresoIncidencias.php
+++ app/views/layouts/Forms/V_IngresoIncidencias.php
@@ -83,7 +83,6 @@ include("app/views/layouts/header/header.php"); // Aquí se incluye la barra lat
                 <div class="form-group">
                     <label for="Foto">Foto</label>
                     <input type="file" class="form-control form-control-lg" name="Foto[]" id="Foto" multiple>
-                    <input type="file" class="form-control" name="Foto" id="Foto">
                 </div>
                 
                 <button type="submit" class="btn btn-primary">Insertar</button>
