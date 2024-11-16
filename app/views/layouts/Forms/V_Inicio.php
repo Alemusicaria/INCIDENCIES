58d139c3535dc9525b1c36af6f563f4faf451708
steve <stevejph48@gmail.com>
2024-11-16 17:57:35 +0100

    dsa

--- app/views/layouts/Forms/V_Inicio.php
+++ app/views/layouts/Forms/V_Inicio.php
@@ -42,17 +42,3 @@ include("app/views/layouts/header/header.php"); // Aquí se incluye la barra lat
         </div>
     </div>
 </body>
-
-
-<!--
-<h1>hola mundo <?php echo $_SESSION['usuario']; ?>!</h1>
-<button onclick="window.location.href='index.php?controller=Incidencias&method=vista_ingreso_incidencias';">
-    Ir a Vista Ingreso de Incidencias
-</button>
-<button onclick="window.location.href='index.php?controller=Registro&method=registro';">
-    Ir a Vista Usuario
-</button>
-<button onclick="window.location.href='index.php?controller=Info_Incidencias&method=mostrar_tabla_incidencias';">
-    Ir a MOSTARAS
-</button>
--->
--- app/views/layouts/menu/menu.php
+++ app/views/layouts/menu/menu.php
@@ -1,66 +1,3 @@
-<!--
-<aside id="sidebar">
-    <div class="d-flex">
-        <button class="toggle-btn" type="button">
-            <i class="lni lni-grid-alt"></i>
-        </button>
-        <div class="sidebar-logo">
-            <a href="#"><?php echo $_SESSION['usuario']; ?></a>
-            <a href="#"><?php echo $_SESSION['rol']; ?></a>
-        </div>
-    </div>
-
-    <div class="fotoperfil">
-        <-- <img src="<?php echo $_SESSION['foto_perfil']; ?>" alt="Foto de perfil"> --
-    </div>
-
-    <ul class="sidebar-nav">
-        <li class="sidebar-item">
-            <a href="perfil.php" class="sidebar-link">
-                <i class="lni lni-user"></i>
-                <span>Perfil</span>
-            </a>
-        </li>
-        <-- MENU DEL ADMIN--
-
-        <?php if ($_SESSION['rol'] == 'Admin'): ?>
-            <li class="sidebar-item">
-                <a href="index.php?controller=Registro&method=registro" class="sidebar-link">
-                    <i class="lni lni-cog"></i>
-                    <span>NOU profesor</span>
-                </a>
-            </li>
-            
-            <li class="sidebar-item">
-                <a href="index.php?controller=Info_Incidencias&method=mostrar_tabla_incidencias" class="sidebar-link">
-                    <i class="lni lni-agenda"></i>
-                    <span>Gestionar Incidència</span>
-                </a>
-            </li>
-
-            <li class="sidebar-item">
-                <a href="index.php?controller=Incidencias&method=vista_ingreso_incidencias" class="sidebar-link">
-                    <i class="lni lni-agenda"></i>
-                    <span>Gestionar Incidència</span>
-                </a>
-            </li>
-        <?php endif; ?>
-
-        <-- MENU DEL PROFESOR--
-
-        <-- MENU DEL TECNICO--
-
-    </ul>
-    <div class="sidebar-footer">
-        <a href="index.php?controller=Login&method=cerrar_sesion" class="sidebar-link">
-            <i class="lni lni-exit"></i>
-            <span>Sortir</span>
-        </a>
-    </div>
-</aside>
-<script src="assets/js/script_default.js"></script>
--->
-
 <!-- Barra superior -->
 <header id="header">
     <div class="header-content">
@@ -142,40 +79,6 @@
 
         <!-- MENU DEL TECNICO-->
         <div class="sidebar-separator">Tecnico</div>
-        
-
-            
-        <!-- 
-                <li class="sidebar-item">
-                    <a href="index.php?controller=Incidencias&method=vista_ingreso_incidencias" class="sidebar-link">
-                        <i class="lni lni-agenda"></i>
-                        <span>NOU profesor</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-cog"></i>
-                        <span>Gestionar professors</span>
-                    </a>
-                </li>
-           
-
-            
-
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-user"></i>
-                    <span>Perfil</span>
-                </a>
-            </li>
-
-            <li class="sidebar-item">
-                <a href="#" class="sidebar-link">
-                    <i class="lni lni-user"></i>
-                    <span>Perfil</span>
-                </a>
-            </li>
-            -->
     </ul>
 
     <div class="sidebar-footer">
