2e09377ab13a9de278f934268c73dbc8fe49b585
Alemusicaria <aleixpratmarin@gmail.com>
2024-11-25 12:28:06 +0100

    sdfgdxfvsc

--- app/views/layouts/header/header.php
+++ app/views/layouts/header/header.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("Location: index.php?controller=Login&method=login");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ca">
 <head>
